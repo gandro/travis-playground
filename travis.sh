067963df1629f38c45bd9a5ade7c1cbb9f6f4764
Sebastian Wicki <gandro@gmx.net>
2015-11-30 22:16:42 +0100

    Fix path

---
+++ travis.sh
@@ -0,0 +1,11 @@
+set -ex
+
+install() {
+  sudo apt-get update
+  sudo apt-get install -y $@
+}
+
+install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev
+wget -nv https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.deb
+sudo dpkg -i vagrant_1.7.4_x86_64.deb
+sudo vagrant up
