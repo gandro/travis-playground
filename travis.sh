6a3f41affa290090aa92db726fd4823ba18fe2ba
Sebastian Wicki <gandro@gmx.net>
2015-11-30 22:41:36 +0100

    Try virtualbox

--- travis.sh
+++ travis.sh
@@ -1,12 +1,7 @@
 set -ex
 
-install() {
-  sudo apt-get update
-  sudo apt-get install -y $@
-}
-
-install libvirt-bin libxslt-dev libxml2-dev libvirt-dev zlib1g-dev
+sudo apt-get update -q
+sudo apt-get install -q virtualbox --fix-missing
 wget -nv https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.deb
 sudo dpkg -i vagrant_1.7.4_x86_64.deb
-sudo vagrant plugin install vagrant-libvirt
-sudo vagrant up --provider=libvirt
+sudo vagrant up
